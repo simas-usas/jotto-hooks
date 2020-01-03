f9121c32e5dbeca3ad8eb25ea6d1361708d8084a
Simas Usas <simas.usas@instarem.com>
2020-01-03 16:55:32 +0200

    old files added

---
+++ src/Congrats.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, checkProps } from '../test/testUtils';
+import Congrats from './Congrats';
+
+const defaultProps = { success: false };
+
+const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props }
+    return shallow(<Congrats {...setupProps} />);
+}
+
+it('renders without error', () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.length).toBe(1);
+});
+
+it('renders no text when "success" prop is false', () => {
+    const wrapper = setup({ success: false });
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.text()).toBe('');
+});
+
+it('renders non-empty congrats message when success prop is true', () => {
+    const wrapper = setup({ success: true });
+    const message = findByTestAttr(wrapper, 'congrats-message');
+    expect(message.text().length).not.toBe(0);
+});
+
+it('does not throw warning with expected props', () => {
+   const expectedProps = { success: false };
+   checkProps(Congrats, expectedProps);
+});
